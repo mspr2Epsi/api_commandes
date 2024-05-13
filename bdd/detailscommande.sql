2e717173a3577bf9db9b4fb13d1ece01e041b398
Asrielbot <49340417+Asrielbot@users.noreply.github.com>
2024-05-13 17:56:47 +0200

    test commande

--- bdd/detailscommande.sql
+++ bdd/detailscommande.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `detailscommande` (
   PRIMARY KEY (`DetailID`),
   KEY `fk_detailsCommande_commandes` (`CommandeID`),
   KEY `fk_detailsCommande_produits` (`ProduitID`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `detailscommande`
