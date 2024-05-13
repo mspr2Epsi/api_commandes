9262f998630dc60ec6b419b098943d32d7265711
Javklin <allvis.gautier@gmail.com>
2024-05-13 14:19:48 +0200

    avancé de l'API commande

--- api_commandes.py
+++ api_commandes.py
@@ -18,5 +18,20 @@ def get_all_orders_with_products_and_clients():
     commande = cursor.fetchall()
     return jsonify({'commande': commande})
 
+@app.route('/commande/<int:commande_id>', methods=['GET'])
+def get__order_with_product_and_client(commande_id):
+    cursor.execute("""
+    SELECT cmd.CommandeID, cmd.DateCommande, cmd.Statut, cmd.PrixTotal, cl.Nom AS NomClient,
+    cl.Prenom AS PrenomClient, pr.Nom AS NomProduit, pr.Description AS DescriptionProduit, pr.PrixUnitaire AS PrixUnitaireProduit, 
+    pr.Stock AS StockProduit, pr.Fournisseur AS FournisseurProduit, dc.Quantite FROM commandes cmd JOIN clients cl 
+    ON cmd.ClientID = cl.ClientID JOIN detailsCommande dc ON cmd.CommandeID = dc.CommandeID 
+    JOIN produits pr ON dc.ProduitID = pr.ProduitID  WHERE cmd.CommandeID = %s;
+    """, (commande_id,))
+    commande = cursor.fetchall()
+    if commande:
+        return jsonify({'client': commande})
+    else:
+        return jsonify({'message': 'Client not found'}), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
